72b23786b2a4c0686ddc4c04555b182708b35820
Shahzaib Khan <shahzaib198@gmail.com>
2017-05-29 16:08:26 +0500

    Fixing the issue of breaking nodes

--- src/js/browser/browser-provenance.js
+++ src/js/browser/browser-provenance.js
@@ -4,8 +4,6 @@
  */
 sm.provenance.browser = function() {
 	
-	/* Delcare Variables */
-	
 	var count = 0;
     const module = {};
 	var recordIDs = {};
@@ -24,16 +22,12 @@ sm.provenance.browser = function() {
         'google.com/url',
         'localhost://'
     ],
-    
-	bookmarkTypes = [ 'auto_bookmark' ],
+    bookmarkTypes = [ 'auto_bookmark' ],
     typedTypes = [ 'typed', 'generated', 'keyword', 'keyword_generated' ];
+
     const dispatch = d3.dispatch('dataChanged');
-	
-	/* Initialize Functions */
-	
     onTabUpdate();
 	onTabCreation();
-
 
 	function onTabCreation() {
 		chrome.tabs.onCreated.addListener( function( tab) {
@@ -41,37 +35,32 @@ sm.provenance.browser = function() {
 			var pid = tab.openerTabId;	
 		  }  
 		  if(pid) {
-			recordIDs[tab.id] = pid;
+			recordIDs[tab.id] = pid; // an edge 
 		  } else {
 			recordIDs[tab.id] = tab.id;
-			addAction(tab,tab.id,0);
+			addAction(tab,tab.id,0); // blank node gets created  - 0 is just written as null value 
 		  }
 		});
 	}
 
     function onTabUpdate() {
         chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+			/* Check for Finish Loading */
+			if(!isFinishLoading(tab,changeInfo)) return;
 			
-			if (isTabIgnored(tab) || isTabInComplete(tab)) return;
-
-			if(changeInfo.status === undefined || changeInfo.status === null) return;
-			
-			if(recordNodeLock[tabId] == 1) {  
-				if(recordNodeHasChild[tabId] == 1) {  
-					addAction(tab,tabId,1);
-				} else {
-					updateAction(tab,tabId);
-				}
+			if(recordNodeLock[tabId] == 1) {  /* node is created and is waiting for information load to complete */
+					if(recordNodeHasChild[tabId] == 1) {  
+						addAction(tab,tabId,1);	
+					} else {
+						updateAction(tab,tabId); //update the node/edge
+					}
 			} else {
-				addAction(tab,tabId,1);
+				addAction(tab,tabId,1); // when a link is added from an old tab this code runs 
 			}
         });
     }
 	
-	/* Support Functions */
-	
 	function updateAction(tab,tabId) {
-			
 		action = {
 			id: recordNodeID[tabId],
 			time: recordNodeTime[tabId],
@@ -83,40 +72,46 @@ sm.provenance.browser = function() {
 			from: recordNodeID[recordIDs[tabId]]
 		};
 		dispatch.dataChanged(action);			
-		recordNodeLock[tabId] = 0; 	
+		recordNodeLock[tabId] = 0; 
+		return ; 
 	}
 	
 	function addAction(tab,tabId,hasChild) {
-		
+			
 			const time = new Date(),
-			
-			action = {
-				id: +time,
-				time: time,
-				url: tab.url,
-				text: tab.title || tab.url || '',
-				type: "link",
-				favIconUrl: tab.favIconUrl,
-				counter: count,
-				from: recordNodeID[recordIDs[tabId]]
-			};
-			
+					action = {
+						id: +time,
+						time: time,
+						url: tab.url,
+						text: tab.title || tab.url || '',
+						type: "link",
+						favIconUrl: tab.favIconUrl,
+						counter: count,
+						from: recordNodeID[recordIDs[tabId]]
+					};
 			recordNodeID[tabId] = +time;
 			recordNodeTime[tabId] = time;
 			recordNodeCounter[tabId] = count;
 			recordNodeLock[tabId] = 1;
 			
-			if(hasChild==1) {
+			
+			if(hasChild == 1) {
 				recordNodeHasChild[recordIDs[tabId]] = 1;
 			}
-		
+			
 			dispatch.dataChanged(action);
-			count++;
+			count++;  
 		
 	}
-	
-	
+
+
 	/* Additional Functions for Checking */
+	
+	function isFinishLoading(tab,changeInfo) {
+        if (isTabIgnored(tab) || isTabInComplete(tab)) return false;
+		if(changeInfo.status === undefined || changeInfo.status === null) return false;
+		return true;
+	}
 	
     function isTabInComplete(tab) {
         return tab.status !== 'complete';
