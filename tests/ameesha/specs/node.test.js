86b87475cdea7de515149e60aa6af89fa6417ff7
Ameesha <ameesha06@gmail.com>
2017-06-25 19:45:33 +0100

    wrote tests for unclear case 2 also

--- tests/ameesha/specs/node.test.js
+++ tests/ameesha/specs/node.test.js
@@ -61,18 +61,58 @@ describe("Unclear case 1: Duplicate URLs", function () {
     });
 });
 
-/*
 describe("Unclear case 2: Old URLs", function () {
     let caseManualUrl = false,
-        caseLink = false;
+        caseLink = false,
+        nodesformed = [],
+        openedurl = 'http://www.mdx.ac.uk/courses',
+                openedurl2 = 'http://www.mdx.ac.uk/global-impact',
 
+        url_opened = false;
+      
     beforeAll(function(done) {
+        
+        chrome.browserAction.onClicked.addListener(function(tab){
+    chrome.tabs.create({ url: openedurl, active: false}, function(tab) {
+            // url_opened = true;        
+
+    });
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    // make sure the status is 'complete' and it's the right tab
+    if (changeInfo.status == 'complete') {
+        url_opened = true;   
+        console.log("done");     
+        done(); 
+    }
+    })
+
+});
+
+    describe("Opened an URL, now calling browser provenance functions and testing", function(){
+
+    // The normal test code becomes very simple because of the asynchronous test.
+   
         const manualUrl = 'http://www.mdx.ac.uk/',
             linkUrl = 'http://www.mdx.ac.uk/our-research'; // Still need to copy this string in content script
 
         let linkClicked = false;
+        beforeAll(function(done){
+        browser = sm.provenance.browser()
+            .on('nodeCreated', function(action) {
+                console.log("node has been created", action.url);
+                if (action.url === manualUrl) caseManualUrl = true;
+                if (action.url === linkUrl) caseLink = true;
 
+                // Call done() when required pages are loaded and ready to run the assertions
+                if (linkClicked){ 
+                  
+                    done()
+                };
+                nodesformed.push(action.url);
 
+            });
         chrome.tabs.create({ url: manualUrl, active: false }, function(tab) {
             // This script will be called in the newly created page
             chrome.tabs.executeScript(tab.id, { code: `
@@ -81,26 +121,15 @@ describe("Unclear case 2: Old URLs", function () {
                 link.click();
             ` }, function() {
                 linkClicked = true;
+                done();
             });
-        });
+        });    
+  
+    })
 
-        browser = sm.provenance.browser()
-            .on('nodeCreated', function(action) {
-                if (action.url === manualUrl) caseManualUrl = true;
-                if (action.url === linkUrl) caseLink = true;
-
-                // Call done() when required pages are loaded and ready to run the assertions
-                if (linkClicked) done();
-            });
-
+    it('Old URL should not appear on history map', function() {
+                  expect(nodesformed.includes(openedurl)).not.toBeTruthy();
     });
-
-    // The normal test code becomes very simple because of the asynchronous test.
-    it('should appear on the history map when a new url is manually entered', function() {
-        expect(caseManualUrl).not.toBeTruthy();
-    });
-    it('should appear on the history map when a link is clicked', function() {
-        expect(caseLink).not.toBeTruthy();
-    });
+    
 });
-*/
+});
