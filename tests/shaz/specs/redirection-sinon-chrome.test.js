dd8fe143829123a9e4d07b2e95dfce7d3ea634f5
Shahzaib Khan <shahzaib198@gmail.com>
2017-07-21 15:32:15 +0500

    Redirection Test Update

--- tests/shaz/specs/redirection-sinon-chrome.test.js
+++ tests/shaz/specs/redirection-sinon-chrome.test.js
@@ -2,13 +2,15 @@
 
 redirectiontest("http://movies.disney.com/finding-nemo","http://www.disneyinternational.com/");
 
+
+
 function redirectiontest(urlstart,urlend) {
 	describe("Case: Redirection", function () {
 		
 		it('Testing: Redirection of URL from one to other', function() {
 
-			const tabInfo = {
-				"tabId": 131,
+			const tabInfo1 = {
+				"tabId": 1637,
 				"changeInfo": {
 					"status": "loading"
 				},
@@ -17,11 +19,176 @@ function redirectiontest(urlstart,urlend) {
 					"audible": false,
 					"autoDiscardable": true,
 					"discarded": false,
-					"height": 780,
+					"height": 513,
 					"highlighted": true,
-					"id": 828,
+					"id": 1637,
 					"incognito": false,
-					"index": 11,
+					"index": 4,
+					"mutedInfo": {
+						"muted": false
+					},
+					"pinned": false,
+					"selected": true,
+					"status": "loading",
+					"title": "movies.disney.com/finding-nemo",
+					"url": "http://movies.disney.com/finding-nemo",
+					"width": 784,
+					"windowId": 1
+				}
+			};
+			
+			chrome.tabs.onUpdated.dispatch(tabInfo1.tabId, tabInfo1.changeInfo, tabInfo1.tab);
+			const lastNode1 = _.last(sm.data.tree.nodes);
+			console.log(lastNode1.text);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
+
+			
+			const tabInfo2 = {
+				"tabId": 1637,
+				"changeInfo": {
+					"status": "loading"
+				},
+				"tab": {
+					"active": true,
+					"audible": false,
+					"autoDiscardable": true,
+					"discarded": false,
+					"height": 513,
+					"highlighted": true,
+					"id": 1637,
+					"incognito": false,
+					"index": 4,
+					"mutedInfo": {
+						"muted": false
+					},
+					"pinned": false,
+					"selected": true,
+					"status": "loading",
+					"title": "Finding Nemo | Official Site | Disney Movies",
+					"url": "http://movies.disney.com/finding-nemo",
+					"width": 784,
+					"windowId": 1
+				}
+			};
+			chrome.tabs.onUpdated.dispatch(tabInfo2.tabId, tabInfo2.changeInfo, tabInfo2.tab);
+			const lastNode2 = _.last(sm.data.tree.nodes);
+			console.log(lastNode2.text);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
+
+
+			const tabInfo3 = {
+				"tabId": 1637,
+				"changeInfo": {
+					"status": "loading"
+				},
+				"tab": {
+					"active": true,
+					"audible": false,
+					"autoDiscardable": true,
+					"discarded": false,
+					"height": 513,
+					"highlighted": true,
+					"id": 1637,
+					"incognito": false,
+					"index": 4,
+					"mutedInfo": {
+						"muted": false
+					},
+					"pinned": false,
+					"selected": true,
+					"status": "loading",
+					"title": "Finding Nemo | Official Site | Disney Movies",
+					"url": "http://www.disneyinternational.com/",
+					"width": 784,
+					"windowId": 1
+				}
+			};
+			
+			chrome.tabs.onUpdated.dispatch(tabInfo3.tabId, tabInfo3.changeInfo, tabInfo3.tab);		
+			var lastNode3 = _.last(sm.data.tree.nodes);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
+
+			const tabInfo4 = {
+				"tabId": 1637,
+				"changeInfo": {
+					"status": "loading"
+				},
+				"tab": {
+					"active": true,
+					"audible": false,
+					"autoDiscardable": true,
+					"discarded": false,
+					"height": 513,
+					"highlighted": true,
+					"id": 1637,
+					"incognito": false,
+					"index": 4,
+					"mutedInfo": {
+						"muted": false
+					},
+					"pinned": false,
+					"selected": true,
+					"status": "loading",
+					"title": "Disney - Disney Online International",
+					"url": "http://www.disneyinternational.com/",
+					"width": 784,
+					"windowId": 1
+				}
+			};
+			chrome.tabs.onUpdated.dispatch(tabInfo4.tabId, tabInfo4.changeInfo, tabInfo4.tab);		
+			const lastNode4 = _.last(sm.data.tree.nodes);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
+			
+			
+			const tabInfo5 = {
+				"tabId": 1637,
+				"changeInfo": {
+					"status": "loading"
+				},
+				"tab": {
+					"active": true,
+					"audible": false,
+					"autoDiscardable": true,
+					"discarded": false,
+					"height": 513,
+					"highlighted": true,
+					"favIconUrl":"http://www.disneyinternational.com/favicon.ico",
+					"id": 1637,
+					"incognito": false,
+					"index": 4,
+					"mutedInfo": {
+						"muted": false
+					},
+					"pinned": false,
+					"selected": true,
+					"status": "loading",
+					"title": "Disney - Disney Online International",
+					"url": "http://www.disneyinternational.com/",
+					"width": 784,
+					"windowId": 1
+				}
+			};
+			
+			chrome.tabs.onUpdated.dispatch(tabInfo5.tabId, tabInfo5.changeInfo, tabInfo5.tab);		
+			lastNode5 = _.last(sm.data.tree.nodes);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
+			
+			const tabInfo6 = {
+				"tabId": 1637,
+				"changeInfo": {
+					"status": "complete"
+				},
+				"tab": {
+					"active": true,
+					"audible": false,
+					"autoDiscardable": true,
+					"discarded": false,
+					"height": 513,
+					"highlighted": true,
+					"favIconUrl":"http://www.disneyinternational.com/favicon.ico",
+					"id": 1637,
+					"incognito": false,
+					"index": 4,
 					"mutedInfo": {
 						"muted": false
 					},
@@ -29,16 +196,17 @@ function redirectiontest(urlstart,urlend) {
 					"selected": true,
 					"status": "complete",
 					"title": "Disney - Disney Online International",
-					"url": urlend,
-					"width": 964,
+					"url": "http://www.disneyinternational.com/",
+					"width": 784,
 					"windowId": 1
 				}
 			};
+			chrome.tabs.onUpdated.dispatch(tabInfo6.tabId, tabInfo6.changeInfo, tabInfo6.tab);		
+			lastNode6 = _.last(sm.data.tree.nodes);
+			console.log("Count in Array:"+sm.data.tree.nodes.length);
 			
-			chrome.tabs.onUpdated.dispatch(tabInfo.tabId, tabInfo.changeInfo, tabInfo.tab);
 			
-			const lastNode = _.last(sm.data.tree.nodes);
-			expect(lastNode.url).not.toBe(urlstart); 
+			
 			
 
 		});
