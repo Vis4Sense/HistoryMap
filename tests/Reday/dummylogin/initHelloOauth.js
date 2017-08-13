b775693416a9aaa869511ac2ff19c538e7c7a3be
RedayY <reday@hotmail.de>
2017-08-13 05:21:25 +0100

    Added functional Hello JS Login
    FInally

--- tests/Reday/dummylogin/initHelloOauth.js
+++ tests/Reday/dummylogin/initHelloOauth.js
@@ -1,3 +1,7 @@
 hello.init({
-    google: "216563153005-vsqoeo6ot311amn4k6dtokn96aj72cth.apps.googleusercontent.com" 
-  });
+  google: '216563153005-rf4vuggusohpth9qm5korfclfo8lah39.apps.googleusercontent.com'
+}, {redirect_uri: 'https://lajblbdkejaddpihfeeihnicfkmiojem.chromiumapp.org/tests/Reday/dummylogin/index.html'})
+
+//redirect URL needs to change
+
+
--- tests/Reday/dummylogin/oauthalpha.js
+++ tests/Reday/dummylogin/oauthalpha.js
@@ -1,63 +1,37 @@
 //external JS to give buttons functionality
 
-        //login Button
-
-var x = document.getElementById("myBtn");
-if(x){
-x.addEventListener("click", myFunction);
-x.addEventListener("click", someOtherFunction);
-
-function myFunction() {
-   document.getElementById("target_aq").style.color('red');
-    console.log("IS THIS DOING ANYTHING AT ALL ?");
-}
-
-function someOtherFunction() {
-    alert ("This function was also executed!")
-    console.log("IS THIS DOING ANYTHING AT ALL ?");
-}
-
-};
-
-  /*      //experiment
-    document.addEventListener('DOMContentLoaded', function() {
-        var link = document.getElementById('logGoogleBTN');
-        link.addEventListener('click', function() {
-            hello('google').login();
-            console.log("IS THIS DOING ANYTHING AT ALL ?");
-    });
+//Jquery for Button action
+$(function(){
+  $('#btn_run').click(function(){get_Info();});
+  $('#btn_logout').click(function(){google_logout();});
 });
 
-        //logout Button
-        var LogoutBTN = document.getElementById('LogOutBTN');
-            LogoutBTN.onclick = function()
-                {
-                    hello('google').logout();
-                    console.log("IS THIS DOING ANYTHING AT ALL ?");
-                };
-*/
+//login
+function get_Info() {
+  hello( 'google' ).login( function() {
+  token = hello( 'google' ).getAuthResponse().access_token;
+});
+}
 
+//logout
+function google_logout() {
+	hello('google').logout().then(function() {
+	alert('Signed out');
+}, function(e) {
+	alert('Signed out error: ' + e.error.message);
+});
+}
 
-
-
-        //Hello.js Initilization
-        hello.init({
-	        google: '216563153005-vsqoeo6ot311amn4k6dtokn96aj72cth.apps.googleusercontent.com'
-        }, {redirect_uri: 'redirect.html'});
-
-        hello.on('auth.login', function(auth) {
-
-	        // Call user information, for the given network
-	        hello(auth.network).api('me').then(function(r) {
-
-		    // Inject it into the container
-		        var label = document.getElementById('profile_' + auth.network);
-		            if (!label) {
-			            label = document.createElement('div');
-			            label.id = 'profile_' + auth.network;
-			            document.getElementById('profile').appendChild(label);
-                    }
-                                
+hello.on('auth.login', function(auth) {
+	// Call user information, for the given network
+	hello(auth.network).api('me').then(function(r) {
+		// Inject it into the container
+		var label = document.getElementById('profile_' + auth.network);
+		if (!label) {
+			label = document.createElement('div');
+			label.id = 'profile_' + auth.network;
+			document.getElementById('profile').appendChild(label);
+		}
 		label.innerHTML = '<img src="' + r.thumbnail + '" /> Hey ' + r.name;
 	});
 });
