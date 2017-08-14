7040099aecd34b9af6da4c70f856f2a13a9baa32
RedayY <reday@hotmail.de>
2017-08-14 16:00:43 +0100

    Removed Login from this Branch
    Removed Login, please use rebuild-Login to test Login features

--- tests/Reday/dummylogin/initHelloOauth.js
+++
@@ -1,7 +0,0 @@
-hello.init({
-  google: '216563153005-rf4vuggusohpth9qm5korfclfo8lah39.apps.googleusercontent.com'
-}, {redirect_uri: 'https://lajblbdkejaddpihfeeihnicfkmiojem.chromiumapp.org/tests/Reday/dummylogin/index.html'})
-
-//redirect URL needs to change
-
-
