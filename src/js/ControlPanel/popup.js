d18761b2e7426e28b4325fb6c6cf43355c74fa07
Kai Xu <k.xu@mdx.ac.uk>
2017-09-05 17:17:07 +0100

    added eslint

--- src/js/ControlPanel/popup.js
+++ src/js/ControlPanel/popup.js
@@ -2,51 +2,52 @@
 //JavaScript Document for SenseMap Control Panel
 
 //Jquery for Button action
-$(function(){
-  $('#btn_login').click(function(){google_Login();});
-  $('#btn_logout').click(function(){google_Logout();});
-  $('#btn_openSense').click(function(){openSensemap();});
+$(function () {
+	$('#btn_login').click(function () { googleLogin(); });
+	$('#btn_logout').click(function () { googleLogout(); });
+	$('#btn_openSense').click(function () { openSensemap(); });
 });
 
-function google_Login(){
+function googleLogin() {
 
-    //HelloJS network identifier
-    var google = hello('google');
+	//HelloJS network identifier
+	var google = hello('google');
 
-	  //Forcing E-Mail out of profile Object
-    google.login({
-      scope: 'email',
-      force: true}).then(function(){
-    return google.api('me');
-            })
-          };
-
-function google_Logout(){
-hello('google').logout().then(function() {
-  alert('Signed out');
-  document.getElementById("btn_logout").disabled = true;
-  document.getElementById("btn_login").style.color = "red";
-  document.getElementById("btn_login").style.visibility = 'visible';
-  location.reload();
-}, function(e) {
-  alert('Signed out error: ' + e.error.message);
-  });
+	//Forcing E-Mail out of profile Object
+	google.login({
+		scope: 'email',
+		force: true
+	}).then(function () {
+		return google.api('me');
+	})
 };
 
-function openSensemap(){
-  chrome.windows.create({'url': 'src/html/historyMapPage.html', 'type': 'popup'}, function(window) {
-   });
+function googleLogout() {
+	hello('google').logout().then(function () {
+		alert('Signed out');
+		document.getElementById("btn_logout").disabled = true;
+		document.getElementById("btn_login").style.color = "red";
+		document.getElementById("btn_login").style.visibility = 'visible';
+		location.reload();
+	}, function (e) {
+		alert('Signed out error: ' + e.error.message);
+	});
 };
 
-window.onload = function button_config(){
-  document.getElementById("btn_logout").disabled = true;
-  document.getElementById("btn_logout").style.color = "red";
-  document.getElementById("btn_logout").style.visibility = 'hidden';
+function openSensemap() {
+	chrome.windows.create({ 'url': 'src/html/historyMapPage.html', 'type': 'popup' }, function (window) {
+	});
+};
 
-  //checks wether HistoryMap is opens, if not open it on launch, if yes then do not open.
-  chrome.tabs.query({'url':'chrome-extension://lajblbdkejaddpihfeeihnicfkmiojem/src/html/historyMapPage.html'}, function(results) {
-    if (results.length == 0) {
-        openSensemap();
-    }
-});
+window.onload = function button_config() {
+	document.getElementById("btn_logout").disabled = true;
+	document.getElementById("btn_logout").style.color = "red";
+	document.getElementById("btn_logout").style.visibility = 'hidden';
+
+	//checks wether HistoryMap is opens, if not open it on launch, if yes then do not open.
+	chrome.tabs.query({ 'url': 'chrome-extension://lajblbdkejaddpihfeeihnicfkmiojem/src/html/historyMapPage.html' }, function (results) {
+		if (results.length == 0) {
+			openSensemap();
+		}
+	});
 };
--- src/js/login/profile.js
+++ src/js/login/profile.js
@@ -3,37 +3,37 @@
 
 
 // Listen to signin requests
-hello.on('auth.login', function(r) {
+hello.on('auth.login', function (r) {
 	// Get Profile
-	hello( r.network ).api( '/me' ).then( function(p) {
-        var label = document.getElementById(r.network);
-		label.innerHTML = "<img src='"+ p.thumbnail + "' id='profileIMG'/> <p id='profileName'>"+ p.name;
+	hello(r.network).api('/me').then(function (p) {
+		var label = document.getElementById(r.network);
+		label.innerHTML = "<img src='" + p.thumbnail + "' id='profileIMG'/> <p id='profileName'>" + p.name;
 		document.getElementById("savSess").innerHTML = "Saved Session";
-        console.log(p);
-        //creating Profile Object for MongoDB useage
-        hello('google').api('me').then(function(json) {
+		console.log(p);
+		//creating Profile Object for MongoDB useage
+		hello('google').api('me').then(function (json) {
 
-            var SenseMapUser = {
-                FirstName : json.first_name,
-                LastName : json.last_name,
-                email : json.email,
-                picture : json.picture,
-                gender : json.gender,
-                language : json.language,
+			var SenseMapUser = {
+				FirstName: json.first_name,
+				LastName: json.last_name,
+				email: json.email,
+				picture: json.picture,
+				gender: json.gender,
+				language: json.language,
 			}
 
-		document.getElementById("btn_login").style.visibility = 'hidden';
-		document.getElementById("btn_logout").disabled = false;
-		document.getElementById("btn_logout").style.color = "darkmagenta";
-		document.getElementById("btn_logout").style.visibility = 'visible';
-		document.getElementById("btn_login").style.color = "red";
+			document.getElementById("btn_login").style.visibility = 'hidden';
+			document.getElementById("btn_logout").disabled = false;
+			document.getElementById("btn_logout").style.color = "darkmagenta";
+			document.getElementById("btn_logout").style.visibility = 'visible';
+			document.getElementById("btn_login").style.color = "red";
 
-        //minor debug
-        //console.log(SenseMapUser);
-});
+			//minor debug
+			//console.log(SenseMapUser);
+		});
 		// On chrome apps we're not able to get remote images
 		// This is a workaround
-		if (typeof(chrome) === 'object') {
+		if (typeof (chrome) === 'object') {
 			img_xhr(label.getElementsByTagName('img')[0], p.thumbnail);
 		}
 	});
