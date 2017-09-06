1eb057ec8efc2d752970708e52dd3e5cfa553668
Kai Xu <k.xu@mdx.ac.uk>
2017-09-06 17:05:23 +0100

    automatic adjust redirect uri with extension id

--- src/js/login/initHelloOauth.js
+++ src/js/login/initHelloOauth.js
@@ -1,6 +1,9 @@
+// chrome.runtime.id returns the id of chrome extension.
+
 hello.init({
   google: '216563153005-rf4vuggusohpth9qm5korfclfo8lah39.apps.googleusercontent.com'
-}, {redirect_uri: 'https://lajblbdkejaddpihfeeihnicfkmiojem.chromiumapp.org/src/html/popup.html'})
+}, {redirect_uri: 'https://'+chrome.runtime.id+'.chromiumapp.org/src/html/popup.html'
+})
 
 //redirect URL needs to change
 
