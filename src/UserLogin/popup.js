51935aeeee8e62afe1c39c55daa134c8c829a085
RedayY <reday@hotmail.de>
2017-11-05 22:22:40 +0000

    hotfix for previous update

--- src/UserLogin/popup.js
+++ src/UserLogin/popup.js
@@ -4,8 +4,8 @@
 hello.init({
     google: '216563153005-rf4vuggusohpth9qm5korfclfo8lah39.apps.googleusercontent.com'
 }, {
-        redirect_uri: 'https://' + chrome.runtime.id + '.chromiumapp.org/src/UserLogin/popup.html'
-    })
+    redirect_uri: 'https://' + chrome.runtime.id + '.chromiumapp.org/src/UserLogin/popup.html'
+})
 
 // Please make sure to update redirect URI to match ur client machine in chrome developer api console.
 
@@ -16,20 +16,7 @@ $(function () {
     });
     $('#btn_logout').click(function () {
         google_Logout();
-    });
-    $('#btn_start').click(function () {
-        UserRecord = true;
-        btn_pause_start_conf();
-    });
-    $('#btn_pause').click(function () {
-        UserRecord = false;
-        btn_pause_start_conf();
-    });
-    $('#btn_new').click(function () {
-        reset_sense();
-    });
-    $('#btn_load').click(function () {
-        load_session();
+        localStorage.clear();
     });
 });
 
@@ -38,9 +25,11 @@ let UserRecord = true;
 let UserEmail;
 let UserProfile;
 let DBnodes = [];
+let ProfileName = "";
+let ProfilePicture = "";
 
 //API (Save and Load) specific Variables
-var baseURL = "https://sensemap-api.herokuapp.com/";
+let baseURL = "https://sensemap-api.herokuapp.com/";
 let APIKey;
 let apiinput;
 let DBSessionPointer;
@@ -75,20 +64,39 @@ function google_Logout() {
     };
 }
 
+// Listens to Login requests and executes functions upon verification
+hello.on('auth.login', function (r) {
+
+    // Get Profiles
+    hello(r.network).api('/me').then(function (p) {
+
+        btn_format();
+        AccLoggedIn = true;
+        UserEmail = p.email;
+        UserProfile = p;
+        saveProfile();
+
+
+        // On chrome apps we're not able to get remote images
+        // This is a workaround
+        if (typeof (chrome) === 'object') {
+            img_xhr(label.getElementsByTagName('img')[0], p.thumbnail);
+        }
+    });
+});
+
+
 function openSensemap() {
     chrome.windows.create({
         'url': 'src/historyMap/historyMap.html',
         'type': 'popup'
-    }, function (window) { });
+    }, function (window) {});
 };
 
 window.onload = function button_config() {
-    document.getElementById("btn_logout").disabled = true;
-    document.getElementById("btn_logout").style.color = "red";
-    document.getElementById("btn_login").style.color = "darkmagenta";
-
-    btn_pause_start_conf();
-
+    saveProfile();
+    btn_reset();
+    //btn_pause_start_conf();
     chrome.tabs.query({
         'url': 'chrome-extension://' + chrome.runtime.id + '/src/historyMap/historyMap.html'
     }, function (results) {
@@ -102,97 +110,19 @@ window.onload = function button_config() {
 function btn_reset() {
     document.getElementById("btn_logout").disabled = true;
     document.getElementById("btn_login").style.color = "darkmagenta";
+    document.getElementById("btn_logout").style.color = "red";
 }
-
-function btn_pause_start_conf() {
-
-    if (UserRecord == true) {
-        document.getElementById("btn_start").disabled = true;
-        document.getElementById("btn_pause").disabled = false;
-        document.getElementById("btn_pause").style.color = "darkmagenta";
-        document.getElementById("btn_start").style.color = "red";
-    } else {
-        document.getElementById("btn_pause").disabled = true;
-        document.getElementById("btn_start").disabled = false;
-        document.getElementById("btn_pause").style.color = "red";
-        document.getElementById("btn_start").style.color = "darkmagenta";
-    }
-
-}
-
-function reset_sense() {
-    var answer = confirm("Would you like to start over again, this do not log you out, does you still wish to Continue?")
-    if (answer) {
-        location.reload();
-    } else {
-        window.alert("SenseMap did not restart, carry on!")
-    }
-}
-
-// Listens to Login requests and executes functions upon verification
-hello.on('auth.login', function (r) {
-
-    // Get Profiles
-    hello(r.network).api('/me').then(function (p) {
-
-        btn_format();
-
-        var value = "";
-        chrome.storage.local.get('Profile-Username', function (result) {
-            value = result;
-        });
-
-
-        if (value == "") {
-
-            //stores Image and Profile E-Mail in localstorage
-
-            profilename = p.name;
-
-            chrome.storage.sync.set({ "ProfileName" : profilename  }, function () {
-                console.log("Profile Saved in Localstorage :" + profilename)
-            });
-
-            draw_profile();
-
-        }
-
-        else {
-
-            var ProfileName = "";
-            var ProfilePicture = "";
-
-            chrome.storage.local.get('ProfileUsername', function (result) {
-                ProfileName = result;
-            });
-
-            chrome.storage.local.get('Profile-Username', function (result) {
-                ProfilePicture = result;
-            });
-
-            document.getElementById("networkName").innerHTML = "<p>" + ProfileName + "</p>";
-            document.getElementById("Image").innerHTML = "<img src='" + ProfilePicture + "'id='profileIMG'/>";
-
-        }
-
-        AccLoggedIn = true;
-        UserEmail = p.email;
-
-
-        // On chrome apps we're not able to get remote images
-        // This is a workaround
-        if (typeof (chrome) === 'object') {
-            img_xhr(label.getElementsByTagName('img')[0], p.thumbnail);
-        }
-    });
-});
 
 function btn_format() {
     document.getElementById("btn_login").disabled = true;
     document.getElementById("btn_logout").disabled = false;
     document.getElementById("btn_logout").style.color = "darkmagenta";
     document.getElementById("btn_login").style.color = "red";
+
 };
+
+
+
 
 function draw_profile() {
     hello('google').api('me').then(function (upp) {
@@ -200,5 +130,34 @@ function draw_profile() {
         document.getElementById("Image").innerHTML = "<img src='" + upp.thumbnail + "'id='profileIMG'/>";
     })
 }
+
+
+
+function saveProfile() {
+
+    if (localStorage.getItem('ProfileName') === null) {
+
+        //stores Image and Profile E-Mail in localstorage
+
+        profilename = UserProfile.name;
+        profileimg = UserProfile.thumbnail;
+        console.log(profilename + " " + profileimg)
+
+        localStorage.setItem("ProfileName", profilename);
+        localStorage.setItem("ProfileIMGURL", profileimg);
+
+        draw_profile();
+
+    } else {
+
+        ProfileName = localStorage.getItem('ProfileName');
+        ProfilePicture = localStorage.getItem('ProfileIMGURL');
+
+        document.getElementById("networkName").innerHTML = "<p>" + ProfileName + "</p>";
+        document.getElementById("Image").innerHTML = "<img src='" + ProfilePicture + "'id='profileIMG'/>";
+
+    }
+}
+
 
 var little_helper = 'https://' + chrome.runtime.id + '.chromiumapp.org/src/UserLogin/popup.html';
