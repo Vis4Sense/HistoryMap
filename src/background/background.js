7a9e6779ddd780bfd06d468442f5f01692629fc4
Kai <k.xu@mdx.ac.uk>
2018-06-19 12:12:07 +0100

    updated extension icon again

--- src/background/background.js
+++ src/background/background.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		// change the extension icon to the coloured one, which should be used when SenseMap is active
 
 		chrome.browserAction.setIcon({
-			path: "/logo/HistoryMapIcon3-16px.png"
+			path: "/logo/historymap16px.png"
 		});
 		const url = chrome.extension.getURL('src/historyMap/historyMap.html');
 		// Only allow a single instance of the history map
