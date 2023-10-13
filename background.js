260a2400beef26e9e6e0b6266cde0ac42d938270
Kai Xu <kxkevin@gmail.com>
2023-10-13 23:10:48 +0100

    clicking the extension icon opens the historymap window.

--- background.js
+++ background.js
@@ -8,17 +8,43 @@ import './sw-import.js'
 //    console.log('HistoryMap: Background service work started.')
 // })
 
-chrome.action.onClicked.addListener( async (tab) => {
-   
-   const prevState = await chrome.action.getBadgeText({tabId: tab.id})
+chrome.action.onClicked.addListener(async (tab) => {
+
+   const prevState = await chrome.action.getBadgeText({ tabId: tab.id })
    const nextState = prevState === 'On' ? 'Off' : 'On'
-   
+
    chrome.action.setBadgeText({
       text: nextState
    })
    console.log("Current tab url: ", tab.url)
+
+   // create the history map window
+   // const url = chrome.extension.getURL("historyMap.html");
+   // Only allow a single instance of the history map
+   // if (getView(url)) {
+   //    return;
+   // }
+   // Create an instance of the history map
+   chrome.windows.create({
+      url: "historyMap.html",
+      type: 'popup',
+      // left: 0,
+      // top: 0,
+      // width: Math.floor(screen.width * 0.33),
+      // height: screen.height
+      // left: 0, // Math.floor(screen.width * 0.5),
+      // top: Math.floor(screen.height * 0.7 + 25),
+      // top: 0,
+      // width: screen.width, // Math.floor(screen.width / 2),
+      // height: Math.floor(screen.height * 0.3 - 25)
+      // height: screen.height
+   }, function (w) {
+      chrome.windows.update(w.id, {
+         focused: true
+      });
+   });
 })
 
 chrome.sidePanel
-   .setPanelBehavior({openPanelOnActionClick:true})
-   .catch( (error) => console.error(error) )
+   .setPanelBehavior({ openPanelOnActionClick: true })
+   .catch((error) => console.error(error))
