9baf7aa33067fbac0f149ac473486b1664bde4e8
Kai Xu <kxkevin@gmail.com>
2023-10-13 21:46:46 +0100

    added service worker

---
+++ background.js
@@ -0,0 +1,18 @@
+// chrome.runtime.onInstalled.addListener( () => {
+//    chrome.action.setBadgeText({
+//       text: "Off"
+//    })
+
+//    console.log('HistoryMap: Background service work started.')
+// })
+
+chrome.action.onClicked.addListener( async (tab) => {
+   
+   const prevState = await chrome.action.getBadgeText({tabId: tab.id})
+   const nextState = prevState === 'On' ? 'Off' : 'On'
+   
+   chrome.action.setBadgeText({
+      text: nextState
+   })
+   console.log("Current tab url: ", tab.url)
+})
