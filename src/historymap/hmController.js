46d06514587cf979d352086bdad49da897f5f9d6
Jeff Pierce <jeffrey.d.pierce@gmail.com>
2023-11-07 09:17:24 -0500

    Standardized on Id

--- src/historymap/hmController.js
+++ src/historymap/hmController.js
@@ -2,49 +2,51 @@ chrome.runtime.onMessage.addListener(
    function (request, sender, sendResponse) {
 
       // There are a few update events when a new page is loaded. changeInfo.status === 'complete' is when the page loading finishes.
-      if (request.data.changeInfo.status === 'complete') {
+      if ( // Protect against errors in form. 
+        request.data &&
+        request.data.changeInfo &&
+        request.data.changeInfo.status === "complete"
+      ) {
+        // debug
+        console.log("page updated: ", request.data);
 
-         // debug
-         console.log('page updated: ', request.data);
-         
-         let newPageId = window.crypto.randomUUID()
+        let newPageId = window.crypto.randomUUID();
 
-         // check if a page opened in this tab before
-         let parentTab = hmTabs.find(t => t.tabId === request.data.tabID)
-         let parentPageID
+        // check if a page opened in this tab before
+        let parentTab = hmTabs.find((t) => t.tabId === request.data.tabID);
+        let parentTabId;
 
-         // create a hmTab object if this is a new tab
-         if (!parentTab) {
-            let newTab = new hmTab(request.data.tabID, newPageId)
-            hmTabs.push(newTab)
+        // create a hmTab object if this is a new tab
+        if (!parentTab) {
+          let newTab = new hmTab(request.data.tabID, newPageId);
+          hmTabs.push(newTab);
 
-            // if the tab is opened by another tab, the 'openerTabId' property will be set
-            let parentTabID = request.data.tab.openerTabId
-            if (parentTabID) {
-               parentPageID = hmTabs.find(t => t.tabId === parentTabID).lastPageID
-            }
-            else {
-               parentPageID = null
-            }
-         }
-         else {
-            parentPageID = parentTab.lastPageID
-         }
+          // if the tab is opened by another tab, the 'openerTabId' property will be set
+          let parentTabId = request.data.tab.openerTabId;
+          if (parentTabId) {
+            parentTabId = hmTabs.find(
+              (t) => t.tabId === parentTabId
+            ).lastPageId;
+          } else {
+            parentTabId = null;
+          }
+        } else {
+          parentPageId = parentTab.lastPageId;
+        }
 
-         // Create a new hmPage object
-         let newPage = new hmPage(
-            newPageId,
-            request.data.tabID,
-            new Date(),
-            request.data.tab,
-            parentPageID
-         )
+        // Create a new hmPage object
+        let newPage = new hmPage(
+          newPageId,
+          request.data.tabID,
+          new Date(),
+          request.data.tab,
+          parentTabId
+        );
 
-         hmPages.push(newPage)
+        hmPages.push(newPage);
 
-         list2tree(hmPages)
-
-         // console.log('%c newPage added: ', 'color:orange', newPage);
+        // Map page data to tree data
+        displayTree(hmPages);
       }
    }
 );
