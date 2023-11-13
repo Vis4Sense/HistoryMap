a7f4db43c17d7aa78b677c85237e9c4071e8d131
Jeff Pierce <jeffrey.d.pierce@gmail.com>
2023-11-13 11:02:31 -0500

    Commented Tabs and react to change URL

--- src/historymap/hmController.js
+++ src/historymap/hmController.js
@@ -1,45 +1,51 @@
 chrome.runtime.onMessage.addListener(
    function (request, sender, sendResponse) {
-
+      // if ( // Protect against errors in form. 
+      //   request.data &&
+      //   request.data.changeInfo &&
+      //   request.data.changeInfo.url
+      // ) {
+      //   console.log("page updated - URL: ", request.data);
+      // }
       // There are a few update events when a new page is loaded. changeInfo.status === 'complete' is when the page loading finishes.
-      if ( // Protect against errors in form. 
+      if (
+        // Protect against errors in form.
+        // request.data &&
+        // request.data.changeInfo &&
+        // request.data.changeInfo.status === "complete"
         request.data &&
         request.data.changeInfo &&
-        request.data.changeInfo.status === "complete"
+        request.data.changeInfo.url
       ) {
         // debug
-        console.log("page updated: ", request.data);
+        console.log("page updated Complete: ", request.data);
 
         let newPageId = window.crypto.randomUUID();
 
         // check if a page opened in this tab before
-        let parentTab = hmTabs.find((t) => t.tabId === request.data.tabID); // NOTE: tabID is not the same case
-        let parentPageId;
+        // let parentTab = hmTabs.find((t) => t.tabId === request.data.tabID); // NOTE: tabID is not the same case
+        // let parentPageId;
 
-        // create a hmTab object if this is a new tab
-        if (!parentTab) {
-          let newTab = new hmTab(request.data.tabID, newPageId);
-          hmTabs.push(newTab);
+        // // create a hmTab object if this is a new tab
+        // if (!parentTab) {
+        //   let newTab = new hmTab(request.data.tabID, newPageId);
+        //   hmTabs.push(newTab);
 
           // if the tab is opened by another tab, the 'openerTabId' property will be set
           let parentTabId = request.data.tab.openerTabId;
           if (parentTabId) {
-
             // Find the parent page
 
             // --- Kai: how is this different from the previous code?
-            const page = hmPages.findLast(
-              (p) => p.tabId == parentTabId
-            )
+            const page = hmPages.findLast((p) => p.tabId == parentTabId);
             parentPageId = page ? page.pageId : null;
             // ---
-
           } else {
             parentPageId = null;
           }
-        } else {
-          parentPageId = parentTab.lastPageId;
-        }
+        // } else {
+        //   parentPageId = parentTab.lastPageId;
+        // }
 
         // Create a new hmPage object
         let newPage = new hmPage(
@@ -54,6 +60,7 @@ chrome.runtime.onMessage.addListener(
 
         // Map page data to tree data
         displayTree(hmPages);
+        // displayTree2(hmPages);
       }
    }
 );
