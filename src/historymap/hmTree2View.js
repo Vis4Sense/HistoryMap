60b7a88ae93675fdb54af7001c4c18deaf1a1916
Kai Xu <kxkevin@gmail.com>
2023-11-16 23:20:39 +0000

    d3 stratify working in tree2view (I hope)

--- src/historymap/hmTree2View.js
+++ src/historymap/hmTree2View.js
@@ -1,96 +1,104 @@
-function displayTree2(dataArray){
-  // Create a root object for the tree
-  const rootId = window.crypto.randomUUID();
-  const root = new hmPage(
-    rootId,
-    null,
-    new Date(),
-    {
-      title: "Tabs opened since opening History Map",
-      label: "History Map",
-    },
-    null
-  );
-  // Link all pages
-  const rootedData = dataArray.map((d) => {
-    d.parentPageId = d.parentPageId ? d.parentPageId : d.pageId == rootId;
-    return d;
-  });
+// Create a root page for the d3 hierarchy as it does not allow multiple roots
+const rootId = window.crypto.randomUUID();
+const root = new hmPage(
+   rootId,
+   null,
+   new Date(),
+   { title: "HistoryMap" },
+   null
+);
+console.log("root: ", root);
 
-  const stratifiedData = d3.stratify()
-  // Set pages without a parentPageId to the root
+function displayTree2(pageArray) {
 
-  const treeData = [root, ...stratifiedData];
-  createTree2(treeData)
+   // Add all page without parent as children of the root
+   const rootedData = pageArray.map((d) => {
+      if (!d.parentPageId) d.parentPageId = rootId;
+      return d;
+   });
+
+   // add the new root to the array
+   rootedData.push(root);
+
+   console.log("rootedData: ", rootedData);
+
+   // create d3 hierarchy with its stratify function
+   const stratifiedData = d3.stratify()
+      .id((d) => d.pageId)
+      .parentId((d) => d.parentPageId)
+      (rootedData);
+   console.log("stratifiedData: ", stratifiedData);
+
+   createTree2(stratifiedData)
 };
 
 
-function createTree2(data){
-  const width = 928;
+function createTree2(data) {
+   const width = 928;
 
-  // Compute the tree height; this approach will allow the height of the
-  // SVG to scale according to the breadth (width) of the tree layout.
-  const root = d3.hierarchy(data);
-  const dx = 10;
-  const dy = width / (root.height + 1);
+   // Compute the tree height; this approach will allow the height of the
+   // SVG to scale according to the breadth (width) of the tree layout.
+   const root = d3.hierarchy(data);
+   const dx = 10;
+   const dy = width / (root.height + 1);
 
-  // Create a tree layout.
-  const tree = d3.tree().nodeSize([dx, dy]);
+   // Create a tree layout.
+   const tree = d3.tree().nodeSize([dx, dy]);
 
-  // Sort the tree and apply the layout.
-  root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
-  tree(root);
+   // Sort the tree and apply the layout.
+   root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
+   tree(root);
 
-  // Compute the extent of the tree. Note that x and y are swapped here
-  // because in the tree layout, x is the breadth, but when displayed, the
-  // tree extends right rather than down.
-  let x0 = Infinity;
-  let x1 = -x0;
-  root.each(d => {
-    if (d.x > x1) x1 = d.x;
-    if (d.x < x0) x0 = d.x;
-  });
+   // Compute the extent of the tree. Note that x and y are swapped here
+   // because in the tree layout, x is the breadth, but when displayed, the
+   // tree extends right rather than down.
+   let x0 = Infinity;
+   let x1 = -x0;
+   root.each(d => {
+      if (d.x > x1) x1 = d.x;
+      if (d.x < x0) x0 = d.x;
+   });
 
-  // Compute the adjusted height of the tree.
-  const height = x1 - x0 + dx * 2;
+   // Compute the adjusted height of the tree.
+   const height = x1 - x0 + dx * 2;
 
-  const svg = d3.create("svg")
+   const svg = d3.create("svg")
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", [-dy / 3, x0 - dx, width, height])
       .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
 
-  const link = svg.append("g")
+   const link = svg.append("g")
       .attr("fill", "none")
       .attr("stroke", "#555")
       .attr("stroke-opacity", 0.4)
       .attr("stroke-width", 1.5)
-    .selectAll()
+      .selectAll()
       .data(root.links())
       .join("path")
-        .attr("d", d3.linkHorizontal()
-            .x(d => d.y)
-            .y(d => d.x));
-  
-  const node = svg.append("g")
+      .attr("d", d3.linkHorizontal()
+         .x(d => d.y)
+         .y(d => d.x));
+
+   const node = svg.append("g")
       .attr("stroke-linejoin", "round")
       .attr("stroke-width", 3)
-    .selectAll()
-    .data(root.descendants())
-    .join("g")
+      .selectAll()
+      .data(root.descendants())
+      .join("g")
       .attr("transform", d => `translate(${d.y},${d.x})`);
 
-  node.append("circle")
+   node.append("circle")
       .attr("fill", d => d.children ? "#555" : "#999")
       .attr("r", 2.5);
 
-  node.append("text")
+   node.append("text")
       .attr("dy", "0.31em")
       .attr("x", d => d.children ? -6 : 6)
       .attr("text-anchor", d => d.children ? "end" : "start")
       .text(d => d.data.name)
-    .clone(true).lower()
+      .clone(true).lower()
       .attr("stroke", "white");
-  
-  return svg.node();
+
+   return svg.node();
 }
