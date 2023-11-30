90790722d6ead7c4d44df3cc6f1ecfd193a12965
Kai Xu <kxkevin@gmail.com>
2023-11-30 23:49:54 +0000

    testing history api search function

--- src/historymap/hmController.js
+++ src/historymap/hmController.js
@@ -11,13 +11,30 @@ chrome.runtime.onMessage.addListener(
 
       // When changeInfo.url
       if (
-         request.data 
-         && request.data.changeInfo 
+         request.data
+         && request.data.changeInfo
          && request.data.changeInfo.title
          && !isIgnoredTab(request.data.tab)
       ) {
          // debug
          console.log("page updated: ", request.data.changeInfo, ', tabId:', request.data.tabID, ', openerTabId: ', request.data.tab.openerTabId, ', data: ', request.data);
+
+         // testing chrome history api
+         chrome.history.search(
+         {
+            'text': '',                // Return every history item....
+            // 'startTime': oneWeekAgo,   // that was accessed less than one week ago.
+            'maxResults': 5            // Optionally state a limit
+         },
+         function (historyItems) {
+            // For each history item, get details on all visits.
+            // for (var i = 0; i < historyItems.length; ++i) {
+            //    var url = historyItems[i].url;
+            //    // do whatever you want with this visited url
+               // console.log(`history item: ${i}, ${historyItems[i]}, transition type: ${historyItems[i].TransitionType}`);
+            // }
+            console.log('history items: ', historyItems);
+         })
 
          let newPageId = window.crypto.randomUUID();
 
@@ -26,14 +43,14 @@ chrome.runtime.onMessage.addListener(
          // if the tab is opened by another tab, the 'openerTabId' property will be set
          if (request.data.tab.openerTabId) {
             // Find the parent page
-            const parentPage = hmPages.findLast((p) => 
+            const parentPage = hmPages.findLast((p) =>
                p.tabId === request.data.tab.openerTabId
             );
             if (parentPage) parentPageId = parentPage.pageId
             // console.log("Opened by a page in a different tab: ", parentPageId);
             // The url changed in the same tab
          } else {
-            const parentPage = hmPages.findLast((p) => 
+            const parentPage = hmPages.findLast((p) =>
                p.tabId === request.data.tabID
             );
             if (parentPage) parentPageId = parentPage.pageId
