52daf4fc05c38f0f2e40782e98aec5890d45e71e
Kai Xu <kxkevin@gmail.com>
2023-12-09 15:59:52 +0000

    refactored the code.

--- src/historymap/hmController.js
+++ src/historymap/hmController.js
@@ -3,13 +3,22 @@ const ignoredUrls = [
    'edge://extensions/'
 ];
 
-function isIgnoredTab(tab) {
-   return ignoredUrls.some(url => tab.url.includes(url));
+function addPage(tabURL, tabID, pageObj, parentPageId) {
+   if (!ignoredUrls.some(url => tabURL.includes(url))) {
+      let newPageId = window.crypto.randomUUID();
+      let newPage = new hmPage(
+         newPageId,
+         tabID,
+         new Date(),
+         pageObj,
+         parentPageId
+      );
+      hmPages.push(newPage);
+      console.log("A new hmPage added:", newPage.pageObj.title, ', ', newPage.pageObj.url);
+      displayTree(hmPages);
+   }
 }
 
-async function getOpenedTabs() {
-   return await chrome.tabs.query({});
-}
 
 chrome.runtime.onMessage.addListener(
    function (request, sender, sendResponse) {
@@ -19,19 +28,7 @@ chrome.runtime.onMessage.addListener(
          chrome.tabs.query({}, function (openedTabs) {
             console.log("Tabs opened before historymap: ", openedTabs);
             for (let i = 0; i < openedTabs.length; i++) {
-               if (!isIgnoredTab(openedTabs[i])) {
-                  let newPageId = window.crypto.randomUUID();
-                  let newPage = new hmPage(
-                     newPageId,
-                     openedTabs[i].id,
-                     new Date(),
-                     openedTabs[i],
-                     null
-                  );
-                  hmPages.push(newPage);
-                  console.log("A new hmPage opened before historymap is added:", newPage.pageObj.title, ', ', newPage.pageObj.url);
-                  displayTree(hmPages);
-               }
+               addPage(openedTabs[i].url, openedTabs[i].id, openedTabs[i], null);
             }
          });
       }
@@ -41,7 +38,6 @@ chrome.runtime.onMessage.addListener(
          request.data
          && request.data.changeInfo
          && request.data.changeInfo.title
-         && !isIgnoredTab(request.data.tab)
       ) {
          // debug
          console.log("tab updated: ", request.data.changeInfo, ', url: ', request.data.tab.url, ', tabId:', request.data.tabID, ', openerTabId: ', request.data.tab.openerTabId, ', data: ', request.data);
@@ -91,21 +87,13 @@ chrome.runtime.onMessage.addListener(
 
                if (isNewPage) {
                   // Create a new hmPage object
-                  let newPage = new hmPage(
-                     newPageId,
-                     request.data.tabID,
-                     new Date(),
-                     request.data.tab,
-                     parentPageId
-                  );
-
-                  hmPages.push(newPage);
-                  console.log("A new hmPage added:", newPage.pageObj.title, ', ', newPage.pageObj.url);
+                  addPage(request.data.tab.url, request.data.tabID, request.data.tab, parentPageId);
                }
-
-               // Map page data to tree data
-               displayTree(hmPages);
-               // displayTree2(hmPages);
+               else {
+                  // Map page data to tree data
+                  displayTree(hmPages);
+                  // displayTree2(hmPages);
+               }
             }
          );
       }
@@ -114,8 +102,8 @@ chrome.runtime.onMessage.addListener(
 
 // When the window is open, the History Map is open
 window.addEventListener("DOMContentLoaded", function () {
-      toggle_badge("Open");
-   });
+   toggle_badge("Open");
+});
 
 window.addEventListener("beforeunload", function () {
    toggle_badge("Off");
