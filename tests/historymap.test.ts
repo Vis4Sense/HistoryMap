d7547593a863f7d5aba39e75d4f586ab5b93a30a
Kai Xu <kxkevin@gmail.com>
2024-01-26 00:24:08 +0000

    playwright is working with a simple popup test.

---
+++ tests/historymap.test.ts
@@ -0,0 +1,11 @@
+import { test, expect } from './fixtures';
+
+test('example test', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page.locator('body')).toHaveText('Changed by my-extension');
+});
+
+test('popup page', async ({ page, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/popup.html`);
+  await expect(page.locator('h1')).toHaveText('History Map');
+});
