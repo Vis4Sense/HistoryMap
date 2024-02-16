ced275d835b777f995b569d3e6d25d13192b742e
Kai Xu <kxkevin@gmail.com>
2024-02-16 12:04:46 +0000

    playwright: switch tab, pause, and close tab

--- tests/historymap.test.ts
+++ tests/historymap.test.ts
@@ -14,5 +14,9 @@ test('HistoryMap page', async ({ page, extensionId, context }) => {
    await page.goto('https://www.bbc.co.uk');
    const hmPage = await context.newPage();
    await hmPage.goto(`chrome-extension://${extensionId}/src/historymap/hm.html`);
+   await page.bringToFront();
+   await hmPage.pause();
+   await page.close();
+   await hmPage.pause();
    await expect(hmPage.locator('h1')).toHaveText('History Map');
  });
