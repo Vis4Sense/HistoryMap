017f7b7b286b5bed7ac8d393cb0ebe61ba92f6f5
Yuhan Guo <72588097+Yuu-Han@users.noreply.github.com>
2024-04-22 20:24:20 +0800

    feat: compact tree layout

--- src/historymap/hmTree3View/hmTree3View.js
+++ src/historymap/hmTree3View/hmTree3View.js
@@ -30,54 +30,51 @@ function displayTree(
    dataArray,
    displayElementId = "svg-div"
 ) {
-   // Create a root object for the tree
-   const rootId = window.crypto.randomUUID();
-   const root = new hmPage(
-      rootId,
-      null,
-      new Date(),
-      {
-         title: "Tabs opened since opening History Map",
-         label: "History Map",
-      },
-      null
-   );
+   var data = [...dataArray],
+      links,
+      width,
+      height,
+      layout;
 
-   // Kai: I am not sure what the code does.
-   const treeData = [root, ...dataArray]
-   const controls = {
-      id: (d, n) => d.pageId,
-      label: (d, n) => (d.pageObj.label ? d.pageObj.label : d.pageObj.title).slice(0,50),
-      // If there is no parent, hang it off the root
-      parentId: (d, n) =>
-      // If there is a parentID return it, if make the parent the root (except for the root)
-      {
-         const pId = d.parentPageId
-            ? d.parentPageId
-            : d.pageId == rootId // If the page is the root, it has no parent
-               ? null // No parent
-               : rootId; // Default to root 
-         // console.log(d, pId);
-         return pId;
-      },
-      title: (d, n) => d.pageObj.title,
-      link: (d, n) => d.pageObj.url,
-      width: 1152,
+   layout = compactTreeLayout();
+   calculateNodeSizes(data);
+   layout.nodes(data).run().close();
 
-      // following controls are added by Yuhan
-      strokeWidth: 1,
-      nodeWidth: 160,
-      nodePaddingX: 20,
-      nodePaddingY: 5,
-   };
+   width = layout.width();
+   height = layout.height();
+   links = layout.links();
 
    const treeComponent = document.querySelector("tree-component");
    const svgDiv = treeComponent.getElementById(displayElementId);
    svgDiv.innerHTML = "";
-   svgDiv.appendChild(Tree3(treeData, controls));
+   svgDiv.appendChild(renderTree({
+      data,
+      links,
+      canvasWidth: width,
+      canvasHeight: height,
+   }));
 }
 
 window.displayTree = displayTree
+
+function calculateNodeSizes(nodes) {
+   const treeComponent = document.querySelector("tree-component");
+   const hiddenDiv = treeComponent.getElementById("hidden-div");
+
+   // Create shadow nodes to get the rendered size of the node
+   nodes.forEach((d) => {
+      const parentNode = d3.create("div")
+         .html(customContent(hmPage2ItemInfo(d))).node();
+      const node = parentNode.firstChild
+      hiddenDiv.appendChild(node);
+      const bbox = node.getBoundingClientRect();
+      const { width, height } = bbox;
+      d.width = width;
+      d.height = height;
+   })
+
+   d3.select(hiddenDiv).selectAll("*").remove();
+}
 
 function playSession(duration=1000) {
    // Starting from empty hmPages
@@ -108,143 +105,20 @@ function hmPage2ItemInfo (hmPage) {
    }
 }
 
-// Yuhan: The layout calculation is temporarily customized to horizontal tree
-// i.e., the root width is set by node height
-
-// Recursively calculate the height of the tree
-function calTreeWidth (root) {
-   let h = 0;
-   // Leaf node
-   if (!("children" in root) || root.children.length == 0) {
-      root["width"] = root.data.nodeSize.height;
-      root["childrenWidth"] = root.data.nodeSize.height;
-      return;
-   }
-   // Internal node
-   for (let i in root.children) {
-     this.calTreeWidth(root.children[i]);
-     h += root.children[i].width;
-   }
-   root["childrenWidth"] = h;
-   // The height of root can be larger than the sum of its children
-   root["width"] = Math.max(h, root.data.nodeSize.height);
-}
-
-// Recursively calculate tree layout, offset means the y position of each subtree,
-// cover means the proportion the subtree occupies in the root tree
-function calTreeLayout (root) {
-   let offset = root.offset;
-   let cover = root.cover;
-   for (let i in root.children) {
-     root.children[i]["offset"] = offset;
-     let c = cover * root.children[i].width / root.childrenWidth;
-     offset += c;
-     root.children[i]["cover"] = c;
-     this.calTreeLayout(root.children[i]);
-   }
-}
-
-function Tree3(
-   data,
+function renderTree(
    {
-      // data is either tabular (array of objects) or hierarchy (nested objects)
-      path, // as an alternative to id and parentId, returns an array identifier, imputing internal nodes
-      id = Array.isArray(data) ? (d) => d.id : null, // if tabular data, given a d in data, returns a unique identifier (string)
-      parentId = Array.isArray(data) ? (d) => d.parentId : null, // if tabular data, given a node d, returns its parent’s identifier
-      children, // if hierarchical data, given a d in data, returns its children
-      tree = d3.tree, // layout algorithm (typically d3.tree or d3.cluster)
-      sort, // how to sort nodes prior to layout (e.g., (a, b) => d3.descending(a.height, b.height))
-      label, // given a node d, returns the display name
-      title, // given a node d, returns its hover text
-      link, // given a node d, its link (if any)
-      linkTarget = "_blank", // the target attribute for links (if any)
-      width = 640, // outer width, in pixels
-      height, // outer height, in pixels
-      r = 3, // radius of nodes
-      padding = 1, // horizontal padding for first and last column
-      fill = "#999", // fill for nodes
-      fillOpacity, // fill opacity for nodes
+      data,
+      links,
+      canvasWidth = 640,
+      canvasHeight = 480,
       stroke = "#555", // stroke for links
       strokeWidth = 1.5, // stroke width for links
       strokeOpacity = 0.4, // stroke opacity for links
       strokeLinejoin, // stroke line join for links
       strokeLinecap, // stroke line cap for links
-      halo = "#fff", // color of label halo
-      haloWidth = 3, // padding around the labels
-      verticalOffset,
       curve = d3.curveBumpX, // curve for the link
-
-      // The following attributes are added by Yuhan
-      nodeWidth = 160, // width of each node
-      nodePaddingX = 20, // horizontal spacing between nodes
-      nodePaddingY = 5, // vertical spacing between nodes
    } = {}
 ) {
-   const treeComponent = document.querySelector("tree-component");
-   const hiddenDiv = treeComponent.getElementById("hidden-div");
-
-   // Create shadow nodes to get the rendered size of the node
-   const shadowNodeRects = data.map((d) => {
-      const parentNode = d3.create("div")
-         .html(customContent(hmPage2ItemInfo(d))).node();
-      const node = parentNode.firstChild
-      hiddenDiv.appendChild(node);
-      const bbox = node.getBoundingClientRect();
-      const { width, height } = bbox;
-      return { width, height };
-   });
-
-   d3.select(hiddenDiv).selectAll("*").remove();
-
-   // Yuhan: assume that all nodes have the same width
-   nodeWidth = shadowNodeRects[0].width || nodeWidth;
-
-   // Yuhan: to enbale spaces between nodes, padding is added to the node size,
-   // it will be removed when drawing the nodes
-   const data_ = data.map((d, i) => {
-      return {
-         ...d,
-         nodeSize: {
-            width: shadowNodeRects[i].width + nodePaddingX * 2,
-            height: shadowNodeRects[i].height + nodePaddingY * 2
-         }
-      }
-   });
-
-   // console.log("tree data", data_)
-
-   const root =
-      path != null
-         ? d3.stratify().path(path)(data_)
-         : id != null || parentId != null
-            ? d3.stratify().id(id).parentId(parentId)(data_)
-            : d3.hierarchy(data_, children);
-
-   // Sort the nodes.
-   if (sort != null) root.sort(sort);
-
-   // Compute labels and titles.
-   const descendants = root.descendants();
-   const L = label == null ? null : descendants.map((d) => label(d.data, d));
-
-   // Compute the layout
-
-   // Yuhan: temporarily I do not use viewbox so the root cover is the exact
-   // height of the node
-   calTreeWidth(root);
-   root["offset"] = 0;
-   root["cover"] = root.width;
-   calTreeLayout(root);
-
-   // Drawing the tree
-
-   // Yuhan: the size of the canvas fits the tree for now, and it is customized
-   // to a horizontal tree
-
-   // Calculate canvas size
-   const canvasWidth = (root.height + 1) * (nodeWidth + nodePaddingX * 2);
-   const canvasHeight = root.width;
-
    // Create svg
    const svg = d3
       .create("svg")
@@ -252,19 +126,6 @@ function Tree3(
       .attr("height", canvasHeight);
 
    // Draw links
-   const links = root.links().map((d) => {
-      const source = d.source;
-      const target = d.target;
-      const sx = (source.depth + 1) * (nodeWidth + nodePaddingX * 2) - nodePaddingX;
-      const sy = source.offset + source.cover / 2;
-      const tx = target.depth * (nodeWidth + nodePaddingX * 2) + nodePaddingX;
-      const ty = target.offset + target.cover / 2;
-      return {
-         source: {...source, x: sx, y: sy},
-         target: {...target, x: tx, y: ty}
-      }
-   });
-
    svg
       .append("g")
       .attr("fill", "none")
@@ -288,28 +149,25 @@ function Tree3(
    const node = svg
       .append("g")
       .selectAll("g")
-      .data(root.descendants())
+      .data(data)
       .join("g")
-      .attr("transform", (d) => `translate(
-         ${(d.depth) * (nodeWidth + nodePaddingX * 2) + nodePaddingX},
-         ${d.offset + d.cover / 2 - d.data.nodeSize.height / 2 + nodePaddingY}
-      )`)
+      .attr("transform", (d) => `translate(${d.x}, ${d.y})`)
       .style("cursor", "pointer")
-      .on("click", (_, d) => handleOpenPage(d.data));
+      .on("click", (_, d) => handleOpenPage(d));
 
    // Node content
    node.append("foreignObject")
-      .attr("width", (d) => d.data.nodeSize.width)
-      .attr("height", (d) => d.data.nodeSize.height)
-      .html((d) => customContent(hmPage2ItemInfo(d.data)));
+      .attr("width", (d) => d.width)
+      .attr("height", (d) => d.height)
+      .html((d) => customContent(hmPage2ItemInfo(d)));
 
    // Forward back icon
    const forwardBack = node
-      .filter((d) => d.data.forwardBack.back > 0)
+      .filter((d) => d.forwardBack.back > 0)
       .append("g")
-      .attr("transform", (d) => `translate(-8, ${d.data.nodeSize.height / 2 - nodePaddingY})`);
+      .attr("transform", (d) => `translate(-8, ${d.height / 2})`);
    forwardBack // forward
-      .filter((d) => d.data.forwardBack.forward > 0)
+      .filter((d) => d.forwardBack.forward > 0)
       .append("path")
       .attr("d", trianglePath(8, "right"))
       .attr("fill", "black")
@@ -326,9 +184,9 @@ function Tree3(
       .attr("y", 3)
       .attr("font-size", 10)
       .style("text-anchor", "middle")
-      .text((d) => `${d.data.forwardBack.back}`);
+      .text((d) => `${d.forwardBack.back}`);
    forwardBack
-      .attr("opacity", d => d.data.isOpened || d.parent.data.isOpened ? 1 : 0.2)
+      .attr("opacity", d => d.isOpened ? 1 : 0.2)
 
    return svg.node();
 }
---
+++ src/historymap/hmTree3View/layout.js
@@ -0,0 +1,118 @@
+// Use mxGraph to compute tree layout
+
+function compactTreeLayout({
+    levelDistance = 30,
+    nodeDistance = 5,
+} = {}) {
+    var module,
+        nodes,
+        links,
+        roots,
+        nodeDict,
+        linkDict,
+        dummyContainer,
+        graph,
+        parentNode,
+        layout;
+    
+    // key functions to bind data
+    var nodeKey = d => d.pageId,
+        linkKey = d => d.source.id + "-" + d.target.id;
+    
+    var parent = d => nodes.find(n => n.pageId === d.parentPageId);
+
+    function initialize() {
+        dummyContainer = document.createElement('div');
+        document.body.appendChild(dummyContainer);
+
+        graph = new mxGraph(dummyContainer);
+        parentNode = graph.getDefaultParent();
+        layout = new mxCompactTreeLayout(graph, true);
+
+        layout.useBoundingBox = false;
+        layout.edgeRouting = false;
+        layout.levelDistance = levelDistance;
+        layout.nodeDistance = nodeDistance;
+
+        nodeDict = {};
+        linkDict = {};
+
+        initializeNodes();
+    }
+
+    function initializeNodes() {
+        if (!nodes) return;
+
+        roots = nodes.filter(d => !parent(d));
+
+        links = nodes.filter(d => parent(d))
+            .map(d => ({
+                source: { id: nodeKey(parent(d)) },
+                target: { id: nodeKey(d) }
+            }));
+
+        nodes.forEach(d => {
+            nodeDict[nodeKey(d)] = graph.insertVertex(parentNode, null, '', 0, 0, d.width, d.height);
+        });
+
+        links.forEach(d => {
+            linkDict[linkKey(d)] = graph.insertEdge(parentNode, null, '', nodeDict[d.source.id], nodeDict[d.target.id]);
+        });
+
+        // virtual root and edges
+        const virtualRoot = graph.insertVertex(parentNode, null, '', 0, 0, -layout.levelDistance - 10, -layout.levelDistance - 10);
+        roots.forEach(r => {
+            graph.insertEdge(parentNode, null, '', virtualRoot, nodeDict[nodeKey(r)]);
+        });
+    }
+
+    function setNodeCoordinate() {
+        nodes.forEach(d => {
+            var m = nodeDict[nodeKey(d)].geometry;
+            d.x = m.x;
+            d.y = m.y;
+        });
+    }
+
+    function setLinkCoordinate() {
+        links.forEach(l => {
+            var source = nodeDict[l.source.id].geometry;
+            var target = nodeDict[l.target.id].geometry;
+            l.source.x = source.x + source.width;
+            l.source.y = source.y + source.height / 2;
+            l.target.x = target.x;
+            l.target.y = target.y + target.height / 2;
+        })
+    }
+
+    initialize();
+
+    return module = {
+        run: function() {
+            layout.execute(parentNode);
+            setNodeCoordinate();
+            setLinkCoordinate();
+            return module;
+        },
+
+        close: function() {
+            d3.select(dummyContainer).remove();
+        },
+
+        nodes: function(_) {
+            return arguments.length ? (nodes = _, initializeNodes(), module) : nodes;
+        },
+
+        links: function() {
+            return links;
+        },
+
+        width: function() {
+            return d3.max(nodes, d => d.x + d.width) || 0;
+        },
+
+        height: function() {
+            return d3.max(nodes, d => d.y + d.height) || 0;
+        }
+    }
+}
