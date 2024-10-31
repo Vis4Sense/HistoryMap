36e702c849ad063e47f1ba5b3175e92a34532911
Yuhan Guo <72588097+Yuu-Han@users.noreply.github.com>
2024-10-31 16:47:42 +0800

    try adding an overlay to webpage and give up

--- src/contentScript/ContentScript.vue
+++ src/contentScript/ContentScript.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="bg-slate-50">
-    Test Content Script
+  <div class="left-[1/2] p-2"
+    :style="{
+      transform: 'translate(-50%, 0)',
+    }"
+  >
+    <div class="text-white"
+      :style="{
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        backdropFilter: 'blur(10px)',
+        width: '50vw',
+        height: '20vh',
+      }"
+    >
+      Test Content Script
+    </div>
   </div>
 </template>
